680e934d8f1479ed15fd9b1bcd67c25bd38a692b
Lars van Vianen <lars@vianen.com>
2024-08-21 22:12:16 +0200

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <hr className="" />
+      <p className="">
+        Created and open sourced by the Stylescape team. Licensed MIT.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
