b05cc7de02adc1c474a157f7dd9983096d691218
Lars van Vianen <lars@vianen.com>
2024-08-25 07:35:51 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 const Footer: React.FC = () => {
-  return (
-    <footer>
-      <hr className="" />
-      <p className="">
-        Created and open sourced by the Stylescape team. Licensed MIT.
-      </p>
-    </footer>
-  );
+    return (
+        <footer>
+            <hr className="" />
+            <p className="">
+                Created and open sourced by the Stylescape team. Licensed MIT.
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
